f99e2791c0889700cf2e3bf72324c01570127981
sebwink <winkler@informatik.uni-tuebingen.de>
2018-06-19 17:31:39 +0200

    fix cast error fully

--- agenspy/graph.py
+++ agenspy/graph.py
@@ -296,7 +296,7 @@ class Graph(agenspy.cursor.Cursor):
                            properties=edge[4])
                  for edge in self.fetchall()]
         node_ids = { edge.sid for edge in edges } | { edge.tid for edge in edges }
-        cmd = ['MATCH (v) WHERE (SELECT CAST(id(v) AS text) in (']
+        cmd = ['MATCH (v) WHERE (SELECT CAST(id(v) AS text)) in (']
         cmd.append(', '.join(["'{}'".format(ID) for ID in node_ids]))
         cmd.append(') RETURN')
         cmd.append(', '.join(['id(v)', 'label(v)', 'properties(v)']))
